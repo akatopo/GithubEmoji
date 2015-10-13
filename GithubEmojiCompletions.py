6c154da20d974b8b6fd95b23a3fb001c525dd0a0
Alex Katopodis <akatopo@users.noreply.github.com>
2015-10-13 19:31:42 +0300

    :sparkles: emoji completions now work on both markdown and COMMIT_EDITMSG files

--- GithubEmojiCompletions.py
+++ GithubEmojiCompletions.py
@@ -1,17 +1,15 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os
 
 settings = None
 
 class GithubEmojiCompletions(sublime_plugin.EventListener):
     """
-    Provide github emoji completions for markdown
+    Provide github emoji completions for selected file extensions or names
     """
 
     def on_query_completions(self, view, prefix, locations):
-        # settings = sublime.load_settings("GithubEmoji.sublime-settings")
-        # Only trigger for markdown
-        if not view.match_selector(locations[0],
-                "text.html.markdown"):
+        # Only trigger for valid file names
+        if not is_valid_file_name(view.file_name()):
             return []
 
         pt = locations[0] - len(prefix) - 1
@@ -22,8 +20,23 @@ class GithubEmojiCompletions(sublime_plugin.EventListener):
             return settings.get("emojiCompletions")
         # emoji completions for commit messages
         if ch == '@':
-            return settings.get("commitEmojiCompletions");
+            return settings.get("commitEmojiCompletions")
         return []
+
+class GithubEmojiAutoCompleteCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("auto_complete")
+        self.view.run_command("left_delete")
+    def is_enabled(self):
+        return is_valid_file_name(self.view.file_name())
+
+def is_valid_file_name(file_name):
+    if file_name is None:
+        return False
+    tail = os.path.split(file_name)[1]
+    ext = os.path.splitext(tail)[1]
+    return (ext in settings.get("emojiFileExtensions") or
+        tail in settings.get("emojiFileNames"))
 
 def plugin_loaded():
     global settings
