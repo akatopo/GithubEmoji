779cc0fd628f7a8587025ceef3df5d938f981516
eugenesvk <eugenesvk@users.noreply.github.com>
2021-09-19 19:19:32 +0700

    :bug: update to work with ST4
    - fixed broken 'tab' commitEmojiCompletions
    - doesn't eat a colon ':' for manually invoked autocompletion
    - doesn't leave extraneous '@' after autocompletion of commitEmojis is triggered (only works in ST4 since you can't use the run_command("left_delete") method, but have to rely on the new ST4 completion matching API)
    
    Fixes: #13

--- GithubEmojiCompletions.py
+++ GithubEmojiCompletions.py
@@ -4,6 +4,8 @@ import os
 
 settings = None
 
+st_version = int(sublime.version())
+invoked_manually = False
 
 class GithubEmojiCompletions(sublime_plugin.EventListener):
     """
@@ -21,23 +23,35 @@ class GithubEmojiCompletions(sublime_plugin.EventListener):
 
         # emoji completions
         if ch == ':':
-            return settings.get("emojiCompletions")
+            if st_version < 4000:
+                return settings.get("emojiCompletions3")
+            else:
+                return settings.get("emojiCompletions")
         # emoji completions for commit messages
         if ch == '@':
-            return settings.get("commitEmojiCompletions")
+            if st_version < 4000:
+                return settings.get("commitEmojiCompletions3")
+            global invoked_manually
+            if invoked_manually:
+                invoked_manually = False
+                return settings.get("commitEmojiCompletions")
+            else:
+                return settings.get("commitEmojiCompletionsAuto")
         return []
-
 
 class GithubEmojiAutoCompleteCommand(sublime_plugin.TextCommand):
     def run(self, edit, isCommitEmoji=False):
+        global invoked_manually
+        invoked_manually = True
         self.view.run_command("auto_complete")
         if isCommitEmoji:
             self.view.run_command("left_delete")
+            if st_version >= 4000:
+                self.view.run_command("insert", {"characters":":"})
 
     def is_enabled(self):
         return is_valid_file_name(self.view.file_name()) or \
             is_valid_scope(self.view, self.view.sel()[0].begin())
-
 
 def is_valid_file_name(file_name):
     if file_name is None:
@@ -46,13 +60,11 @@ def is_valid_file_name(file_name):
 
     return tail in settings.get("emojiFileNames")
 
-
 def is_valid_scope(view, point):
     for scope in settings.get("emojiScopes"):
         if view.match_selector(point, scope):
             return True
     return False
-
 
 def plugin_loaded():
     global settings
